f7ee67f35389f73d043e0f379e5486ecd38e21d5
Bobinette <xavier.chassin@live.fr>
2018-04-30 20:15:07 +0200

    Add math support with katex

--- app/src/components/markdown/index.js
+++ app/src/components/markdown/index.js
@@ -4,8 +4,13 @@ import PropTypes from 'prop-types';
 import remark from 'remark';
 import remarkEmoji from 'remark-emoji';
 import remarkReact from 'remark-react';
+import remarkMath from 'remark-math';
+import remarkKatex from 'remark-html-katex';
+
 import githubSanitize from 'hast-util-sanitize/lib/github.json';
 import merge from 'deepmerge';
+
+import camelCase from 'lodash.camelcase';
 
 import todayReference, { handler } from './references';
 import tag from './tags';
@@ -15,20 +20,36 @@ import TodayReference from './components/today-reference';
 import './markdown.scss';
 
 const createElement = (name, attrs, children) => {
+  let eltName = name;
+  let attributes = attrs;
+
   switch (name) {
+    // Because hast-to-hyperscript kebab-cases the attributes, I have to re-camelCase
+    // them in order for the svg in katex to work...
+    case 'svg':
+    case 'path':
+      attributes = {}; // reset
+      Object.keys(attrs).forEach(key => {
+        attributes[camelCase(key)] = attrs[key];
+      });
+      break;
+
+    // Transform the titles into simple strong tags
     case 'h1':
     case 'h2':
     case 'h3':
     case 'h4':
     case 'h5':
     case 'h6':
-      return React.createElement('strong', attrs, children);
+      eltName = 'strong';
+      break;
+    // Custom elements:
     case 'TodayReference':
       return <TodayReference autoLoad={attrs['auto-load']} {...attrs} />;
     default:
       break;
   }
-  return React.createElement(name, attrs, children);
+  return React.createElement(eltName, attributes, children);
 };
 
 export const Markdown = ({ text, className, autoLoadReferences }) => {
@@ -36,9 +57,11 @@ export const Markdown = ({ text, className, autoLoadReferences }) => {
     attributes: {
       '*': ['className'],
       TodayReference: ['uuid', 'autoLoad'],
-      span: ['className'],
+      span: ['className', 'style'],
+      svg: ['height', 'width', 'viewBox', 'preserveAspectRatio'],
+      path: ['d'],
     },
-    tagNames: ['TodayReference', 'span'],
+    tagNames: ['TodayReference', 'span', 'svg', 'path'],
   });
   try {
     const md = remark()
@@ -47,6 +70,8 @@ export const Markdown = ({ text, className, autoLoadReferences }) => {
         autoLoad: autoLoadReferences,
       })
       .use(tag)
+      .use(remarkMath)
+      .use(remarkKatex)
       .use(remarkReact, {
         createElement,
         sanitize,
